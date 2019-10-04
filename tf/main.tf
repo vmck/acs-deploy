4d88be5bd387fcadcc3aaa704088f4753c7a8430
Alex Morega <alex@grep.ro>
2019-10-04 16:49:57 +0300

    Render terraform config

---
+++ tf/main.tf
@@ -0,0 +1,11 @@
+# <nomad.Provider 'http://10.42.2.1:4646'>
+provider "nomad" {
+  address = "http://10.42.2.1:4646"
+  version = "~> 1.4"
+}
+
+# <nomad.Job 'acs-interface'>
+resource "nomad_job" "acs-interface" {
+  jobspec = "${file("${path.module}/../templates/interface.nomad")}"
+}
+
